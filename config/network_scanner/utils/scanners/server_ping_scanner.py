df5917e3400632ae72db51234e83377fc26a2e40
Mohammad Shapouri <mohammadshapouri02@gmail.com>
2024-08-18 00:45:54 +0330

    Changed ping method. Now it works with both Linux and Windows.

--- config/network_scanner/utils/scanners/server_ping_scanner.py
+++ config/network_scanner/utils/scanners/server_ping_scanner.py
@@ -1,6 +1,7 @@
 import re
 import random
-import subprocess
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
 import concurrent.futures
 from network_scanner.models import NetworkScanningSessionIPAddress
 
@@ -81,7 +82,18 @@ class ServerPingScanner:
 
     def ping(self, ip_addr_obj, packet_count):
         # Returning 0 means that the sever is up.
-        return subprocess.call(["ping", "-c", str(packet_count), str(ip_addr_obj.ip_address)]) == 0
+        """
+        Returns True if host (str) responds to a ping request.
+        Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+        """
+
+        # Option for the number of packets as a function of
+        param = '-n' if platform.system().lower()=='windows' else '-c'
+
+        # Building the command. Ex: "ping -c 1 google.com"
+        command = ['ping', param, str(packet_count), str(ip_addr_obj.ip_address)]
+
+        return subprocess.call(command) == 0
 
 
 
